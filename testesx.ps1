8f2ad8215b05fd9aae2885c6078c367ef5313501
ctnocpatchteam <89206118+ctnocpatchteam@users.noreply.github.com>
2021-11-09 20:03:27 +0800

    Update testesx.ps1

--- testesx.ps1
+++ testesx.ps1
@@ -1,22 +1,49 @@
 
+[Net.ServicePointManager]::SecurityProtocol = [Net.SecurityProtocolType]::Tls12
+function Unzip($zipfile, $outdir)
+{
+    Add-Type -AssemblyName System.IO.Compression.FileSystem
+    $archive = [System.IO.Compression.ZipFile]::OpenRead($zipfile)
+    try
+    {
+        foreach ($entry in $archive.Entries)
+        {
+            $entryTargetFilePath = [System.IO.Path]::Combine($outdir, $entry.FullName)
+            $entryDir = [System.IO.Path]::GetDirectoryName($entryTargetFilePath)
 
-# Variables
-$modulesdir = 'C:\Modules\'
+            #Ensure the directory of the archive entry exists
+            if(!(Test-Path $entryDir )){
+                New-Item -ItemType Directory -Path $entryDir | Out-Null 
+            }
 
-# If PowerCLI module exist, do nothing, if it doesn't, download and install!
-if (Get-Module -ListAvailable -Name VMware.PowerCLI) {
-    Write-Host "Module already exists, no need to download."
-} 
-else {
-    Write-Host "Module does not exist, downloading!"
-    Save-Module -Name VMware.PowerCLI -Path C:\Modules\
-    Install-Module -Name VMware.PowerCLI
+            #If the entry is not a directory entry, then extract entry
+            if(!$entryTargetFilePath.EndsWith("\")){
+                [System.IO.Compression.ZipFileExtensions]::ExtractToFile($entry, $entryTargetFilePath, $true);
+            }
+        }
+    }
+    finally
+    {
+        $archive.Dispose()
+    }
 }
 
-# If modules directory does not exist, make directory
-if (!($modulesdir | Test-Path)) { 
-    mkdir C:\Modules\
-}
+$Url = 'https://code.vmware.com/docs/14830' 
+$ZipFile = 'C:\ct-agent\' + $(Split-Path -Path $Url -Leaf) 
+$Destination = 'c:\Windows\system32\WindowsPowerShell\v1.0\Modules\' 
+ 
+Invoke-WebRequest -Uri $Url -OutFile $ZipFile 
+
+Unzip -zipfile $ZipFile -outdir $Destination
+ 
+cd c:\Windows\system32\WindowsPowerShell\v1.0\Modules
+
+Get-ChildItem * -Recurse | Unblock-File
+
+clear 
+
+Get-Module -Name VMware.PowerCLI -ListAvailable
+
 
 # Set to ignore invalid ssl certificates if self-certificate
 Set-PowerCLIConfiguration -InvalidCertificateAction Ignore -Confirm:$false
